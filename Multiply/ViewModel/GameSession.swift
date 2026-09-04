a81133929d46f74637ca7604f3161e56579928f3
roundeeesss <yaroslavgoncharov31@gmail.com>
2026-09-04 17:57:24 +0200

    Create GameSession struct to handle logic

--- Multiply/ViewModel/GameSession.swift
+++ Multiply/ViewModel/GameSession.swift
@@ -1,7 +1,20 @@
-//
-//  QuestionViewModel.swift
-//  Multiply
-//
-//  Created by Yaroslav on 9/2/26.
-//
-struct
+
+struct GameSession {
+    let questions: [Question]
+    var currentQuestionIndex = 0
+    var correctAnswers = 0
+
+    var currentQuestion: Question {
+        questions[currentQuestionIndex]
+    }
+
+    init(chosenNumber: Int, numberOfQuestions: Int) {
+        questions = (0..<numberOfQuestions).map {
+            Question(
+                chosenNumber: chosenNumber,
+                multiplier: Int.random(in: 1...12),
+                index: $0 + 1
+            )
+        }
+    }
+}
